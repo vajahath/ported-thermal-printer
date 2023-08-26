3ec9f7559830dbd3e63eb6daba0bf3ab431e9eb3
Vajahath Ahmed <vajuoff.1@gmail.com>
2023-08-27 01:17:53 +0530

    removed the web component stuff

--- index.mjs
+++ index.mjs
@@ -1,13 +1,3 @@
 import { renderPrintContent } from "./printer-web-component.mjs";
 
 console.log(await renderPrintContent());
-// window.customElements.define("printer-element", PrinterElement);
-
-// document.getElementById("test-button").addEventListener("click", () => {
-//   console.log("clicked");
-//   document
-//     .getElementById("printer-element")
-//     .dispatchEvent(
-//       new CustomEvent("print", { detail: { stuff: "other stuff" } })
-//     );
-// });
--- printer-web-component.jsx
+++ printer-web-component.jsx
@@ -13,28 +13,17 @@ export async function renderPrintContent() {
   const data = await render(
     <Printer type="epson">
       <Text>Hello World</Text>
+      <Text>Hello World</Text>
+      <Text>Hello World</Text>
+      <Text>Hello World</Text>
+      <Text>Hello World</Text>
+      <Text>Hello World</Text>
+      <Text>Hello World</Text>
+      <Text>Hello World</Text>
+      <Text>Hello World</Text>
+      <Text>Hello World</Text>
+      <Text>Hello World</Text>
     </Printer>
   );
   return data;
-}
-
-export class PrinterElement extends HTMLElement {
-  constructor() {
-    super();
-  }
-
-  handler(event) {
-    console.log("From Handler. Event:", event);
-    renderPrintContent().then(console.log).catch(console.error);
-  }
-
-  connectedCallback() {
-    console.log("connectedCallback");
-    this.addEventListener("print", this.handler.bind(this));
-  }
-
-  disconnectedCallback() {
-    console.log("disconnectedCallback");
-    this.removeEventListener("print", this.handler.bind(this));
-  }
 }
